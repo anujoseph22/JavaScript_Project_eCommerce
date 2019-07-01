57125306bc7f15922ab5b00c302e39ff186de167
anu <anna.anujoseph@gmail.com>
2019-07-02 00:35:13 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("sidebarbtn").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("sidebarbtn").style.marginLeft = "0";
+}
+
+
